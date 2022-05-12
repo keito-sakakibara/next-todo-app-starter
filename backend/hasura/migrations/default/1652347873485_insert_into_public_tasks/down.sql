8a1aa62ee2871b8bae5af0ae0a93c6950982695a
KazukiHayase <k.hayase@buysell-technologies.com>
2022-05-12 18:36:08 +0900

    seedデータ用意

---
+++ backend/hasura/migrations/default/1652347873485_insert_into_public_tasks/down.sql
@@ -0,0 +1,5 @@
+DELETE FROM "public"."tasks" WHERE "id" = 5;
+DELETE FROM "public"."tasks" WHERE "id" = 4;
+DELETE FROM "public"."tasks" WHERE "id" = 3;
+DELETE FROM "public"."tasks" WHERE "id" = 2;
+DELETE FROM "public"."tasks" WHERE "id" = 1;
---
+++ backend/hasura/migrations/default/1652347873485_insert_into_public_tasks/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."tasks"("id", "name", "created_at", "updated_at", "description") VALUES (1, E'タスク1', E'2022-05-12T18:31:13.460952+09:00', E'2022-05-12T18:31:13.460952+09:00', E'タスク1です');
+INSERT INTO "public"."tasks"("id", "name", "created_at", "updated_at", "description") VALUES (2, E'タスク2', E'2022-05-12T18:31:13.460952+09:00', E'2022-05-12T18:31:13.460952+09:00', E'タスク2です');
+INSERT INTO "public"."tasks"("id", "name", "created_at", "updated_at", "description") VALUES (3, E'タスク3', E'2022-05-12T18:31:13.460952+09:00', E'2022-05-12T18:31:13.460952+09:00', E'タスク3です');
+INSERT INTO "public"."tasks"("id", "name", "created_at", "updated_at", "description") VALUES (4, E'タスク4', E'2022-05-12T18:31:13.460952+09:00', E'2022-05-12T18:31:13.460952+09:00', E'タスク4です');
+INSERT INTO "public"."tasks"("id", "name", "created_at", "updated_at", "description") VALUES (5, E'タスク5', E'2022-05-12T18:31:13.460952+09:00', E'2022-05-12T18:31:13.460952+09:00', E'タスク5です');
